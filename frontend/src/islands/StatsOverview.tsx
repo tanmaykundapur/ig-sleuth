8cdf74f2c2d527b3d4b52227ca4ae4c497fcb971
Tanmay Kundapur <tanmaykundapur@gmail.com>
2026-07-14 22:03:53 -0400

    add StatsOverview component skeleton

---
+++ frontend/src/islands/StatsOverview.tsx
@@ -0,0 +1,15 @@
+interface StatsOverviewProps {
+    totalFollowing: number;
+    totalFollowers: number;
+    mutualCount: number;
+    notFollowingBackCount: number;
+    notFollowedBackByYouCount: number;
+}
+
+export default function StatsOverview(data: StatsOverviewProps) {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    );
+}
