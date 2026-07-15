c3023acae37ed60ed03c6ae34fe074e54757db15
Tanmay Kundapur <tanmaykundapur@gmail.com>
2026-07-14 20:13:16 -0400

    add a basic list card for non-followers and update Dashboard

--- frontend/src/islands/Dashboard.tsx
+++ frontend/src/islands/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { ConnectionsAnalysisResult } from "../lib/types";
+import NotFollowingBackHero from "./NotFollowingBackHero";
 import UploadForm from "./UploadForm";
 
 export default function Dashboard() {
@@ -27,7 +28,9 @@ export default function Dashboard() {
             {result && (
                 <div className="w-full flex flex-col gap-8">
                     {/* dashboard components go here, e.g.: */}
-                    {/* <NotFollowingBackHero usernames={result.not_following_back} /> */}
+                    <NotFollowingBackHero
+                        usernames={result.not_following_back}
+                    />
                 </div>
             )}
         </div>
---
+++ frontend/src/islands/NotFollowingBackHero.tsx
@@ -0,0 +1,30 @@
+export default function NotFollowingBackHero({
+    usernames,
+}: {
+    usernames: string[];
+}) {
+    return (
+        <div className="w-full max-w-lg mx-auto bg-white border border-gray-200 rounded-2xl shadow-sm p-6 md:p-8">
+            <div className="pb-3">
+                <h1 className="text-xl font-bold">
+                    {usernames.length} don't follow you back
+                </h1>
+                <h2 className="text-md text-gray-800 pb-1">
+                    (Ur a fan 😭 They do NOT know you)
+                </h2>
+            </div>
+            {usernames && (
+                <ul className="max-h-60 overflow-y-auto">
+                    {usernames.map((username, index) => (
+                        <li
+                            className="m-1 p-4 border border-gray-200 rounded-2xl hover:font-bold"
+                            key={index}
+                        >
+                            {username}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
